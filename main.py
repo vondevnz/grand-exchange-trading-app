552c916a5a6867a19e17484cdc3519c700abb780
vondev <fergus@vondev.co.nz>
2026-07-07 12:11:21 +0100

    added uv package manager, first API get request to wiki

---
+++ main.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+def main():
+
+    url = "https://prices.runescape.wiki/api/v1/osrs/latest?id=4151"
+
+    load_dotenv()
+    user_agent = os.getenv("USER_AGENT_TEXT")
+
+    headers = {
+        "User-Agent": user_agent
+    }
+    
+    response = requests.get(url, headers = headers)
+
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
