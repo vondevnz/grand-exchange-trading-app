bfdb14df38cdd70d88a17aa5a3f639d2f78b5884
vondev <fergus@vondev.co.nz>
2026-07-08 01:39:21 +0100

    added fastAPI and created API backend

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
 from dotenv import load_dotenv
 import os
 import requests
+import json
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-def main():
+load_dotenv()
+USER_AGENT = os.getenv("USER_AGENT_TEXT")
 
-    url = "https://prices.runescape.wiki/api/v1/osrs/latest?id=4151"
+app = FastAPI()
 
-    load_dotenv()
-    user_agent = os.getenv("USER_AGENT_TEXT")
+# Allow your React dev server to call this API
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Vite's default dev port
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+
+@app.get("/api/prices/latest")
+def get_latest_prices():
     headers = {
-        "User-Agent": user_agent
+        "User-Agent": USER_AGENT
     }
-    
-    #response = requests.get(url, headers = headers)
 
-    #print(response.text)
+    # Load the original JSON file
+    with open("mappings/items.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
 
+    item_id = data["Yew sapling"]
 
-if __name__ == "__main__":
-    main()
+    url = f"https://prices.runescape.wiki/api/v1/osrs/latest?id={item_id}"
+
+    response = requests.get(url, headers = headers)
+
+    response.raise_for_status()
+
+    print(response.text)
+
+    return response.json()
